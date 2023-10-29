50bb6196fce4cf817c7762416dde5990c2076f71
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-10-29 18:56:00 +0700

    create augmentation module
    -random flip left to right images in dataset

---
+++ augmentation.py
@@ -0,0 +1,60 @@
+import tensorflow as tf
+import os
+import json
+import random
+
+
+# Path of original images
+folder_path = './test/test data'
+
+# Path of Label
+labels_file = './test\data.json'
+
+# Load image labels from the JSON file
+with open(labels_file, 'r') as f:
+    image_labels = json.load(f)
+
+# Create a folder to store augmented images
+augmented_folder_path = './test/test data'
+os.makedirs(augmented_folder_path, exist_ok=True)
+
+# Loop through the image files, apply data augmentation
+for image_file in os.listdir(folder_path):
+    if image_file.endswith(('.jpg', '.jpeg', '.png', '.bmp')):
+        # Load the image
+        image_path = os.path.join(folder_path, image_file)
+        image = tf.io.read_file(image_path)
+        image = tf.image.decode_image(image)
+
+        # Get the image filename
+        image_filename = os.path.basename(image_file)
+
+        # Get the labels for the image from the JSON file
+        image_label = image_labels.get(image_filename, {})
+
+        # Generate unique seeds
+        seed = tf.random.uniform(shape=(2,), maxval=10000, dtype=tf.int32)
+
+        # Apply data augmentation to the image with unique seeds for each attribute
+        augmented_image = tf.image.stateless_random_flip_left_right(image, seed=seed)
+
+        # Define the filename for the augmented image
+        augmented_image_filename = f'augmented_images_test_{image_filename}'
+
+        # Save the augmented image as a JPEG file in the augmented folder
+        augmented_image_path = os.path.join(augmented_folder_path, augmented_image_filename)
+        tf.io.write_file(augmented_image_path, tf.image.encode_jpeg(augmented_image))
+
+        # Update the image labels with the augmented image's filename and labels
+        augmented_image_label = {augmented_image_filename: image_label}
+        image_labels.update(augmented_image_label)
+
+        # Print the image label and the path where the augmented image is saved
+        print(f"Image Label: {image_label}")
+        print(f"Augmented image saved as {augmented_image_path}\n")
+
+# Save the updated labels to the JSON file
+with open(labels_file, 'w') as f:
+    json.dump(image_labels, f, indent=4)
+
+print("Updated JSON file with augmented image labels.")
