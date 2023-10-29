a4bc3a1a2519cd216f7126057b9ae8971cbdaef7
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-10-29 19:14:32 +0700

    add contrast randomize

--- augmentation.py
+++ augmentation.py
@@ -3,15 +3,16 @@ import os
 import json
 import random
 
-def augment(image, seed_left_right,seed_up_down,seed_brightness,seed_saturation):
+def augment(image, seed_left_right,seed_up_down,seed_brightness,seed_saturation,seed_contrast):
     image = tf.image.stateless_random_flip_left_right(image, seed=seed_left_right)
     image = tf.image.stateless_random_flip_up_down(image, seed=seed_up_down)
     image = tf.image.stateless_random_brightness(image, max_delta=0.2, seed=seed_brightness)
     image = tf.image.stateless_random_saturation(image, lower=0.5, upper=1.5, seed=seed_saturation)
+    image = tf.image.stateless_random_contrast(image, lower=0.7, upper=1.3, seed=seed_contrast)
     return image
 
 # Path of original images
-folder_path = './test/test data'
+folder_path = 'D:\my dataset\images_150'
 
 # Path of Label
 labels_file = './test\data.json'
@@ -21,7 +22,7 @@ with open(labels_file, 'r') as f:
     image_labels = json.load(f)
 
 # Create a folder to store augmented images
-augmented_folder_path = './test/test data'
+augmented_folder_path = r'D:\my dataset\testaug'
 os.makedirs(augmented_folder_path, exist_ok=True)
 
 # Loop through the image files, apply data augmentation
@@ -43,10 +44,11 @@ for image_file in os.listdir(folder_path):
         seed_up_down = tf.random.uniform(shape=(2,), maxval=10000, dtype=tf.int32)
         seed_brightness = tf.random.uniform(shape=(2,), maxval=10000, dtype=tf.int32)
         seed_saturation = tf.random.uniform(shape=(2,), maxval=10000, dtype=tf.int32)
+        seed_contrast = tf.random.uniform(shape=(2,), maxval=10000, dtype=tf.int32)
 
 
         # Apply data augmentation to the image with unique seeds for each attribute
-        augmented_image = augment(image,seed_left_right,seed_up_down,seed_brightness,seed_saturation)
+        augmented_image = augment(image,seed_left_right,seed_up_down,seed_brightness,seed_saturation,seed_contrast)
 
         # Define the filename for the augmented image
         augmented_image_filename = f'augmented_images_test_{image_filename}'
