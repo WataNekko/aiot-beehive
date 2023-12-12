b1cc92ac1f243a87956f2440ed8a68cb51736fac
TNDC2002 <71067277+TNDC2002@users.noreply.github.com>
2023-12-12 20:55:00 +0700

    Update Object_Detection.py
    delete unecessary

--- yolo/yolov7/Object_Detection.py
+++ yolo/yolov7/Object_Detection.py
@@ -1,165 +1,91 @@
-import argparse
-import time
-from pathlib import Path
-import os
-import cv2
 import torch
-import torch.backends.cudnn as cudnn
 from numpy import random
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
-    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.datasets import LoadImages
+from utils.general import check_img_size, non_max_suppression, scale_coords
 from utils.plots import plot_one_box
-from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+from utils.torch_utils import select_device, TracedModel
 
 
-def detect(save_img=True, weights = "D:/best.pt", source = "D:/dataset/test/images/d1ea6407-f000000017_png.rf.f292506a34acc49cfda2ebe1b05e097d.jpg",save_dir = "D:/tmp/yolov7/runs/detect/exp"):
+imgsz=480
+conf_thres=0.3
+iou_thres=0.3
+device=''
+classes=None
+agnostic_nms=False
+# Directories
+# increment run
 
-    imgsz=480
-    conf_thres=0.3
-    iou_thres=0.3
-    device=''
-    view_img=False
-    save_txt=False
-    save_conf=False
-    nosave=False
-    classes=None
-    agnostic_nms=False
-    augment=True
-    update=False
-    project='runs/detect'
-    name='exp'
-    exist_ok=False
-    no_trace=False
+# Initialize
+# set_logging()
+device = select_device("cuda" if torch.cuda.is_available() else "cpu")
+half = device.type != 'cpu'  # half precision only supported on CUDA
 
-    org_img = cv2.imread(source)
-    # Directories
-    # increment run
-    os.makedirs(save_dir, exist_ok=True)  # make dir
+# Load model
+WEIGHTS = "D:/best.pt"
+model = attempt_load(WEIGHTS, map_location=device)  # load FP32 model
+stride = int(model.stride.max())  # model stride
+imgsz = check_img_size(imgsz, s=stride)  # check img_size
 
-    # Initialize
-    # set_logging()
-    device = select_device("cuda" if torch.cuda.is_available() else "cpu")
-    half = device.type != 'cpu'  # half precision only supported on CUDA
+if True:
+    model = TracedModel(model, device, imgsz)
 
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+if half:
+    model.half()  # to FP16
 
-    if True:
-        model = TracedModel(model, device, imgsz)
 
-    if half:
-        model.half()  # to FP16
+# Get names and colors
+names = model.module.names if hasattr(model, 'module') else model.names
+colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
 
-    # Second-stage classifier
-    classify = False
-    if classify:
-        modelc = load_classifier(name='resnet101', n=2)  # initialize
-        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+# Run inference
+if device.type != 'cpu':
+    model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
 
+
+def detect(source):
     # Set Dataloader
     dataset = LoadImages(source, img_size=imgsz, stride=stride)
 
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
-    colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
-
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    old_img_w = old_img_h = imgsz
-    old_img_b = 1
-
-    t0 = time.time()
-    for path, img, im0s, vid_cap in dataset:
+    for path, img, im0s, _ in dataset:
+        
         img = torch.from_numpy(img).to(device)
         img = img.half() if half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
         if img.ndimension() == 3:
             img = img.unsqueeze(0)
-
-        # Warmup
-        if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
-            old_img_b = img.shape[0]
-            old_img_h = img.shape[2]
-            old_img_w = img.shape[3]
-            for i in range(3):
-                model(img, augment=True)[0]
-
+        
         # Inference
-        t1 = time_synchronized()
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
             pred = model(img, augment=True)[0]
-        t2 = time_synchronized()
 
         # Apply NMS
         pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms)
-        t3 = time_synchronized()
-
-        # Apply Classifier
-        if classify:
-            pred = apply_classifier(pred, modelc, img, im0s)
 
         # Process detections
-        for i, det in enumerate(pred):  # detections per image
+        for det in pred:  # detections per image
             p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
-
-
-            save_path = os.path.join(save_dir, os.path.basename(path))  # img.jpg
-            # txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            
+            
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
-                print(det)
                 boundingbox = det.int().detach().cpu().numpy()
-                print(boundingbox[0][:4])
-                _1st_bb = boundingbox[0][:4]
-                number_of_bee = len(det)
-                print(number_of_bee)
-                # org_img[_1st_bb[1]:_1st_bb[3], _1st_bb[0]:_1st_bb[2]]
-                # cv2.imwrite("test.png",org_img[_1st_bb[1]:_1st_bb[3], _1st_bb[0]:_1st_bb[2]])
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
-
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
-                        # with open(txt_path + '.txt', 'a') as f:
-                        #     f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    label = f'{names[int(cls)]} {conf:.2f}'
+                    plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
 
-                    if save_img or view_img:  # Add bbox to image
-                        label = f'{names[int(cls)]} {conf:.2f}'
-                        plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
-
-            # Print time (inference + NMS)
-            print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
-
-            # Stream results
-            # if view_img:
-            #     cv2.imshow(str(p), im0)
-            #     cv2.waitKey(1)  # 1 millisecond
-
-            # Save results (image with detections)
-            if save_img:
-                if dataset.mode == 'image':
-                    cv2.imwrite(save_path, im0)
-                    print(f" The image with the result is saved in: {save_path}")
-
-    if save_txt or save_img:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        #print(f"Results saved to {save_dir}{s}")
-
-    print(f'Done. ({time.time() - t0:.3f}s)')
+                return boundingbox, im0
 
 
 
-detect()
+
+a,b=detect("D:/dataset/test/images/d1ea6407-f000000017_png.rf.f292506a34acc49cfda2ebe1b05e097d.jpg")
+
+print(a)
+import cv2
+cv2.imshow('1',b)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
